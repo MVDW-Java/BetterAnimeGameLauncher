a014336e4a3075cc9573bd2c35945c362bcb6f94
MVDW <maxvdwolf@gmail.com>
2023-11-28 22:32:22 +0100

    add initDXVK call in runner

--- BetterAnimeGameLauncher/cli/runner.py
+++ BetterAnimeGameLauncher/cli/runner.py
@@ -1,9 +1,12 @@
 from BetterAnimeGameLauncher import *
 
-from BetterAnimeGameLauncher.component.wine import initWine
 from BetterAnimeGameLauncher.util.metadata import getComponentMetadata
 from BetterAnimeGameLauncher.util.cache import getCache, saveCache
 from BetterAnimeGameLauncher.util.config import getConfig
+
+from BetterAnimeGameLauncher.component.wine import initWine
+from BetterAnimeGameLauncher.component.dxvk import initDXVK
+
 
 def runner(args):
     # Get required data from server, cache and config
@@ -15,5 +18,6 @@ def runner(args):
         match att:
             case "wine":
                 initWine(val);
-            
+            case "dxvk":
+                initDXVK(val);
             
