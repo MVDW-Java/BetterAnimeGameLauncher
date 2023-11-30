f4c84b0153248a56ec6f69bbe884446c7c8e8b0f
MVDW <maxvdwolf@gmail.com>
2023-11-30 23:42:12 +0100

    rewritten the genshin installer

--- BetterAnimeGameLauncher/game/genshin.py
+++ BetterAnimeGameLauncher/game/genshin.py
@@ -1,9 +1,12 @@
 from BetterAnimeGameLauncher import *
 
+from BetterAnimeGameLauncher.util.download import download_file
+
 import os
 import base64
 import requests
 import json
+import hashlib
 
 # Lauch game
 def launchGenshin():
@@ -12,7 +15,7 @@ def launchGenshin():
 
     # test options
     CONFIG["lang"] = ["en-us"]
-    CONFIG["installed_genshin_ver"] = "4.0.0"
+    CONFIG["installed_genshin_ver"] = "3.1.0"
 
     # basic game info
     game_version = api_responce["data"]["game"]["latest"]["version"]
@@ -34,47 +37,120 @@ def launchGenshin():
 # Install game
 def installGenshin(api_responce):
 
-    # create game directory
-    if not os.path.exists(PATH_DATA_GAME_GENSHIN_DIR):
-        os.makedirs(PATH_DATA_GAME_GENSHIN_DIR)
+    # local information
+    installed_version = CONFIG["installed_genshin_ver"] = "3.1.0"
+    language_list = CONFIG["lang"] = ["en-us"]
 
-    # Default values
-    list_voicepacks = api_responce["data"]["game"]["latest"]["voice_packs"]
 
-    game_download_list = []
-    voice_download_list = []
+    # base game information
+    base_game_version = api_responce["data"]["game"]["latest"]["version"]
+    base_game_url = api_responce["data"]["game"]["latest"]["path"]
+    voicepack_game_list = api_responce["data"]["game"]["latest"]["voice_packs"]
+    
+    download_list = []
     has_upgrade = False
+    game_basefilename = None
 
-    # check if there is an upgrade avalable
-    if(CONFIG["installed_genshin_ver"] != None):
+    print("Checking current base game installation status... (This may take some time)")
+
+
+    # check if it can just be upgraded instead redownloading the full game
+    if(installed_version != None):
         for diff in api_responce["data"]["game"]["diffs"]:
-            if(diff["version"] == CONFIG["installed_genshin_ver"]):
-                list_voicepacks = diff["voice_packs"]
-                game_download_list.append(diff["path"])
+            if(diff["version"] == installed_version):
+                filename = os.path.basename(segment["path"])
+                game_basefilename = os.path.splitext(filename)[0]
+
+                download_obj = {}
+
+                download_obj["url"] = diff["path"]
+                download_obj["md5"] = diff["md5"]
+
+                download_list.append(download_obj)
+                voicepack_game_list = diff["voice_packs"]
+
                 has_upgrade = True
 
     # when no upgrade avalable
     if not has_upgrade:
         for segment in api_responce["data"]["game"]["latest"]["segments"]:
-            game_download_list.append(segment["path"])
+
+            filename = os.path.basename(segment["path"])
+            game_basefilename = os.path.splitext(filename)[0]
+            location = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, filename)
+
+            if not (os.path.exists(location)) and False : #or (hashlib.md5(open(location,'rb').read()).hexdigest() != segment["md5"])
+                download_obj = {}
+                download_obj["url"] = segment["path"]
+                download_obj["md5"] = segment["md5"]
+
+                download_list.append(download_obj)
+
+
+    print("Checking current voice pack installation status... (This may take some time)")
 
     # queue voicepacks
-    for voice in list_voicepacks:
-        if(voice["language"] in CONFIG["lang"]):
-            voice_download_list.append(voice["path"])
+    for voice in voicepack_game_list:
+
+        filename = os.path.basename(voice["path"])
+        location = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, filename)
+
+        if(voice["language"] in language_list) and False:  #and (not (os.path.exists(location)) or (hashlib.md5(open(location,'rb').read()).hexdigest() != voice["md5"]))
+            download_obj = {}
+
+            download_obj["url"] = voice["path"]
+            download_obj["md5"] = voice["md5"]
+
+            download_list.append(download_obj)
+
+
+    # Download all files
+    for download in download_list:
+        
+        filename = os.path.basename(download["url"])
+        location = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, filename)
+
+        print("downloading: ", filename)
+        
+
+    # if there is files in parts, combine them into 1 big file
+    print("Combining zip to make it readable...")
+    location = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, game_basefilename)
+    parts = getZipParts(location)
+
+    if len(parts) > 0:
+        with open(location, "ab") as outfile:
+            for part_filename in parts:
+                part_location = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, part_filename)
+                with open(part_location, "rb") as infile:
+                    while True:
+                        chunk = infile.read(8192)
+                        if not chunk:
+                            break
+                        outfile.write(chunk)
+
+
+    print(game_basefilename)
+    print(getZipParts(location))
 
 
 
-    print("GAME DOWNLOAD: ", game_download_list)
-    print("VOICE DOWNLOAD: ", voice_download_list)
 
+def getZipParts(zip_file):
+    files = []
+    
+    game_basefilename = os.path.basename(zip_file)
 
+    for num in range(1,999):
+        part_file =  f"{game_basefilename}.{str(num).zfill(3)}"
+        path = os.path.join(PATH_DATA_GAME_GENSHIN_DIR, part_file)
 
-    #print(game_executable)
+        if os.path.exists(path):
+            files.append(part_file)
+        else:
+            break
 
-
-
-
+    return files
 
 
 
---
+++ BetterAnimeGameLauncher/util/download.py
@@ -0,0 +1,10 @@
+
+import requests
+
+def download_file(url, destination):
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        with open(destination, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
