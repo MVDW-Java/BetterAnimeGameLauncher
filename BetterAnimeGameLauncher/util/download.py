ca72663b0466f61fe26a0d17439eeeda2f068470
MVDW <maxvdwolf@gmail.com>
2023-12-10 13:08:33 +0100

    fixed download_file() not creating files

--- BetterAnimeGameLauncher/util/download.py
+++ BetterAnimeGameLauncher/util/download.py
@@ -4,7 +4,7 @@ import requests
 def download_file(url, destination):
     with requests.get(url, stream=True) as response:
         response.raise_for_status()
-        with open(destination, 'wb') as file:
+        with open(destination, 'ab') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 if chunk:
                     file.write(chunk)
