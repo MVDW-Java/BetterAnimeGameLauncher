dff46e496502128c27b1c69034a9a59544ed1ac3
MVDW <maxvdwolf@gmail.com>
2023-12-10 14:22:26 +0100

    add jadeite patch support

--- BetterAnimeGameLauncher/__init__.py
+++ BetterAnimeGameLauncher/__init__.py
@@ -21,6 +21,7 @@ PATH_DATA_DIR = os.path.join(os.environ.get('APPDATA') or os.environ.get('XDG_DA
 PATH_DATA_PREFIX_DIR = os.path.join(PATH_DATA_DIR, "prefix")
 PATH_DATA_WINE_DIR = os.path.join(PATH_DATA_DIR, "wine")
 PATH_DATA_DXVK_DIR = os.path.join(PATH_DATA_DIR, "dxvk")
+PATH_DATA_JADEITE_DIR = os.path.join(PATH_DATA_DIR, "jadeite")
 PATH_DATA_GAME_DIR = os.path.join(PATH_DATA_DIR, "game")
 
 PATH_DATA_GAME_GENSHIN_DIR = os.path.join(PATH_DATA_GAME_DIR, "genshin_impact")
