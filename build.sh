c02e4939dbbf3c9ee8d15d1bb92b8fd5b1981a2d
MVDW <maxvdwolf@gmail.com>
2023-12-11 11:49:40 +0100

    add install parameter to buildscript

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 pyinstaller -D -F -F -n BAGL --add-data "./BetterAnimeGameLauncher:." -c ./BetterAnimeGameLauncher/__main__.py
+if [[ $1 == "install" ]]; then
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root"
+	else
+		cp "$(pwd)/dist/BAGL" "/usr/local/bin"
+		ln -s /usr/local/bin/BAGL /usr/local/bin/bagl
+	fi
+fi
 echo "--------------"
 echo "output in: $(pwd)/dist" 
