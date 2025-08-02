a1ef9fbe14d13d07be8d1f5696988c2931f2d759
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-08-02 23:46:48 +0300

    Update __init__.py

--- lib/bagl/__init__.py
+++ lib/bagl/__init__.py
@@ -6,16 +6,18 @@ APP_NAME_SHORT = "BAGL"
 APP_DESC = ""
 
 PATH_CONFIG_DIR = os.path.join(os.environ.get('APPDATA') or os.environ.get('XDG_CONFIG_HOME') or os.path.join(os.environ['HOME'], '.config'), APP_NAME)
-PATH_CONFIG_FILE = os.path.join(PATH_CONFIG_DIR, "config.yaml");
+PATH_CONFIG_FILE = os.path.join(PATH_CONFIG_DIR, "config.yaml")
 CONFIG = {}
 CONFIG_LOADED = False
 
 PATH_CACHE_DIR = os.path.join(os.environ.get('APPDATA') or os.environ.get('XDG_CACHE_HOME') or os.path.join(os.environ['HOME'], '.cache'), APP_NAME)
-PATH_CACHE_FILE = os.path.join(PATH_CACHE_DIR, "cache.json");
+PATH_CACHE_FILE = os.path.join(PATH_CACHE_DIR, "cache.json")
 CACHE = {}
 CACHE_LOADED = False
 
 METADATA = {}
+
+API_URL = "aHR0cHM6Ly9zZy1oeXAtYXBpLmhveW92ZXJzZS5jb20vaHlwL2h5cC1jb25uZWN0L2FwaS9nZXRHYW1lUGFja2FnZXM/Z2FtZV9pZHNbXT01VElWdnZjd3RNJmdhbWVfaWRzW109Z29wUjZDdWZyMyZnYW1lX2lkc1tdPXdrRTVQNVdzSWYmZ2FtZV9pZHNbXT11eEI0TUM3bnpDJmdhbWVfaWRzW109ZzBtTUl2c2hEYiZnYW1lX2lkc1tdPTR6aXlzcVhPUTgmZ2FtZV9pZHNbXT1VNWhiZHNUOVc3JmdhbWVfaWRzW109YnhQVFhTRVQ1dCZsYXVuY2hlcl9pZD1WWVRwWGxiV284"
 
 PATH_DATA_DIR = os.path.join(os.environ.get('APPDATA') or os.environ.get('XDG_DATA_HOME') or os.path.join(os.environ['HOME'], '.local', 'share'), APP_NAME)
 PATH_DATA_PREFIX_DIR = os.path.join(PATH_DATA_DIR, "prefix")
@@ -25,51 +27,86 @@ PATH_DATA_JADEITE_DIR = os.path.join(PATH_DATA_DIR, "jadeite")
 PATH_DATA_GAME_DIR = os.path.join(PATH_DATA_DIR, "game")
 
 PATH_DATA_GAME_GENSHIN_DIR = os.path.join(PATH_DATA_GAME_DIR, "genshin_impact")
-PATH_DATA_GAME_HONKAI3RD_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd")
+PATH_DATA_GAME_HONKAI3RD_GLOBAL_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd_global")
+PATH_DATA_GAME_HONKAI3RD_TAIWAN_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd_taiwan")
+PATH_DATA_GAME_HONKAI3RD_KOREA_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd_korea")
+PATH_DATA_GAME_HONKAI3RD_JAPAN_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd_japan")
+PATH_DATA_GAME_ZZZ_DIR = os.path.join(PATH_DATA_GAME_DIR, "zenless_zone_zero")
 PATH_DATA_GAME_STARRAIL_DIR = os.path.join(PATH_DATA_GAME_DIR, "honkai_starrail")
 PATH_DATA_GAME_PGR_DIR = os.path.join(PATH_DATA_GAME_DIR, "punishing_gray_raven")
 PATH_DATA_GAME_RE1999_DIR = os.path.join(PATH_DATA_GAME_DIR, "reverse_1999")
 
 GAME_MAP = {
     0: {
-        "NAME": "Honkai Impact 3rd",
-        "NAME_SHORT": "HI3",
-        "NAME_CONFIG": "honkai_impact_3rd",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "honkai_3rd")
+        "NAME": "Honkai Impact 3rd Global",
+        "NAME_SHORT": "HI3GLB",
+        "NAME_CONFIG": "honkai_impact_3rd_global",
+        "GAME_ID": "5TIVvvcwtM",
+        "DATA_DIR": PATH_DATA_GAME_HONKAI3RD_GLOBAL_DIR,
         
     },
     1: {
+        "NAME": "Honkai Impact 3rd Taiwan",
+        "NAME_SHORT": "HI3TWN",
+        "NAME_CONFIG": "honkai_impact_3rd_taiwan",
+        "GAME_ID": "wkE5P5WsIf",
+        "DATA_DIR": PATH_DATA_GAME_HONKAI3RD_TAIWAN_DIR,
+        
+    },
+    2: {
+        "NAME": "Honkai Impact 3rd Korea",
+        "NAME_SHORT": "HI3KOR",
+        "NAME_CONFIG": "honkai_impact_3rd_korea",
+        "GAME_ID": "uxB4MC7nzC",
+        "DATA_DIR": PATH_DATA_GAME_HONKAI3RD_KOREA_DIR,
+        
+    },
+    3: {
+        "NAME": "Honkai Impact 3rd Japan",
+        "NAME_SHORT": "HI3JPN",
+        "NAME_CONFIG": "honkai_impact_3rd_japan",
+        "GAME_ID": "g0mMIvshDb",
+        "DATA_DIR": PATH_DATA_GAME_HONKAI3RD_JAPAN_DIR,
+        
+    },
+    4: {
         "NAME": "Genshin Impact",
         "NAME_SHORT": "GI",
         "NAME_CONFIG": "genshin_impact",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "genshin_impact"),
-        "API": "aHR0cHM6Ly9zZGstb3Mtc3RhdGljLmhveW92ZXJzZS5jb20vaGs0ZV9nbG9iYWwvbWRrL2xhdW5jaGVyL2FwaS9yZXNvdXJjZT9rZXk9Z2NTdGdhcmgmbGF1bmNoZXJfaWQ9MTA="
+        "GAME_ID": "gopR6Cufr3",
+        "DATA_DIR": PATH_DATA_GAME_GENSHIN_DIR,
+        
     },
-    2: {
+    5: {
         "NAME": "Honkai: Star Rail",
         "NAME_SHORT": "HSR",
         "NAME_CONFIG": "honkai_starrail",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "honkai_starrail"),
-        "API": "aHR0cHM6Ly9oa3JwZy1sYXVuY2hlci1zdGF0aWMuaG95b3ZlcnNlLmNvbS9oa3JwZ19nbG9iYWwvbWRrL2xhdW5jaGVyL2FwaS9yZXNvdXJjZT9jaGFubmVsX2lkPTEma2V5PXZwbE9WWDhWbjdjd0c4eWImbGF1bmNoZXJfaWQ9MzU="
+        "GAME_ID": "4ziysqXOQ8",
+        "DATA_DIR": PATH_DATA_GAME_STARRAIL_DIR,
+        
     },
-    3: {
+    6: {
         "NAME": "Zenless Zone Zero",
         "NAME_SHORT": "ZZZ",
         "NAME_CONFIG": "zenless_zone_zero",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "zenless_zone_zero")
+        "GAME_ID": "U5hbdsT9W7",
+        "DATA_DIR": PATH_DATA_GAME_ZZZ_DIR,
+        
     },
-    4: {
-        "NAME": "Punishing: Gray Raven",
-        "NAME_SHORT": "PGR",
-        "NAME_CONFIG": "punishing_gray_raven",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "punishing_gray_raven")
-    },
-    5: {
-        "NAME": "Reverse: 1999",
-        "NAME_SHORT": "RE1999",
-        "NAME_CONFIG": "reverse_1999",
-        "DATA_DIR": os.path.join(PATH_DATA_GAME_DIR, "reverse_1999")
-    }
+    # 7: {
+    #     "NAME": "Punishing: Gray Raven",
+    #     "NAME_SHORT": "PGR",
+    #     "NAME_CONFIG": "punishing_gray_raven",
+    #     "DATA_DIR": PATH_DATA_GAME_PGR_DIR,
+        
+    # },
+    # 8: {
+    #     "NAME": "Reverse: 1999",
+    #     "NAME_SHORT": "RE1999",
+    #     "NAME_CONFIG": "reverse_1999",
+    #     "DATA_DIR": PATH_DATA_GAME_RE1999_DIR,
+        
+    # }
 }
 
 PATH_MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
