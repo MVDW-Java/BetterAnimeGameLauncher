38f0b2ac7d02eba94a788baad07e79d4114a14e9
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-08-10 19:31:44 +0300

    Update test.py

--- lib/bagl/game/test.py
+++ lib/bagl/game/test.py
@@ -1,22 +1,6 @@
-from bagl import *
-
-from bagl.util.download import download_file
-from bagl.util.cache import saveCache
-from bagl.util.config import saveConfig
-
 from bagl.manager.api.hoyoverse import Hoyoverse
 
-
-
-import os
-import base64
-import requests
-import json
-import hashlib
-import zipfile
-
-
-# Lauch game
+# Launch game
 def launchTest():
-    game = Hoyoverse(2)
+    game = Hoyoverse(3)
     print(game.listDownload())
